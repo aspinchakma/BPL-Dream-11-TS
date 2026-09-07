c1a33ac9dc85892a8827b980a94adbdc9a3e942a
Aspin Chakma <aspinchakma2019@gmail.com>
2026-09-07 23:23:43 +0600

    finished banner section

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -9,17 +9,17 @@ const Banner = () => {
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
       }}
-      className="bg-black"
+      className="bg-black py-16 rounded-2xl"
     >
       <img className="mx-auto" src={bannerIMG} alt="" />
-      <h1 className="text-center font-bold text-4xl text-white">
+      <h1 className="text-center font-bold text-4xl text-white my-7">
         Assemble Your Ultimate Dream 11 Cricket Team
       </h1>
-      <p className="text-2xl text-center text-[#B9B9B9]">
+      <p className="text-2xl text-center text-[#B9B9B9] mb-4">
         Beyond Boundaries Beyond Limits
       </p>
-      <div>
-        <button className="bg-[#d7ec27] font-medium rounded-xl px-3 py-3">
+      <div className="border-2 border-[#d7ec27] p-2 rounded-xl w-fit mx-auto">
+        <button className="bg-[#d7ec27] font-bold rounded-xl px-5 py-3 cursor-pointer hover:text-[#d7ec27] transition duration-500 hover:bg-transparent">
           Claim Free Credit
         </button>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <div className="w-[85%] mx-auto border border-black mt-28 flex-1">
+      <div className="w-[85%] mx-auto  mt-28 flex-1">
         <Banner />
       </div>
       <h3 className="font-bold text-5xl">Footer</h3>
